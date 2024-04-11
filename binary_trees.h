a4839eaed612ffac4ce598bbda0743b743921537
probrahim <ibrahimhassani674@gmail.com>
2024-04-12 00:21:05 +0200

    adva 0

--- binary_trees.h
+++ binary_trees.h
@@ -34,6 +34,8 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 
+
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
@@ -47,6 +49,13 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/*   advanced */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+
+
+
 
 #endif /* _binary_trees_h_ */
 
