3bb2322fbb81c6e0534bef1691e5b084cc47926b
probrahim <ibrahimhassani674@gmail.com>
2024-04-12 00:30:45 +0200

    task 22

--- binary_trees.h
+++ binary_trees.h
@@ -54,6 +54,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 
 
