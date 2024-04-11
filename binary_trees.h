c984cfe8aec4ab0d9b6e536f92666fa7c57a6483
probrahim <ibrahimhassani674@gmail.com>
2024-04-11 22:01:54 +0200

    task 11

--- binary_trees.h
+++ binary_trees.h
@@ -30,6 +30,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 /* Prints a binary tree */
 void binary_tree_print(const binary_tree_t *tree);
